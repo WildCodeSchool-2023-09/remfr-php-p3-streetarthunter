012b1b1673b0d87e2a895210003c9c214b2fcd4b
Daniel Lemos <daniel.lemos.jose1998@gmail.com>
2024-01-12 17:28:16 +0100

    Branchement du formulaire a la BDD

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,19 +4,28 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/', name: 'app_')]
 class ContactController extends AbstractController
 {
     #[Route('contact', name: 'contact')]
-    public function new(): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $contact = new Contact();
-
         $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
 
         return $this->render('contact/index.html.twig', ['form' => $form]);
     }
