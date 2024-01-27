4ed838a722c972c0b116e5cae8dce6da56cb3d00
Daniel Lemos <daniel.lemos.jose1998@gmail.com>
2024-01-27 15:19:51 +0100

    modifications de la page profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,8 +11,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProfilController extends AbstractController
 {
     #[Route('/profil/', name: 'user_profil')]
-    public function index(): Response
+    public function index(User $user, UserRepository $userRepository): Response
     {
-        return $this->render('user/profil.html.twig');
+        return $this->render('user/profil.html.twig', [
+        ]);
     }
 }
