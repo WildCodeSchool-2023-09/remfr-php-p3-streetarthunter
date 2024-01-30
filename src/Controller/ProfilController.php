fd91d5d6478fb8969279dc4dcfd3c7a6fde3b48e
Daniel Lemos <daniel.lemos.jose1998@gmail.com>
2024-01-30 15:21:19 +0100

    page profil dynamique

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,17 +3,36 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ProfilType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ProfilController extends AbstractController
 {
-    #[Route('/profil/', name: 'user_profil')]
-    public function index(User $user, UserRepository $userRepository): Response
-    {
+    #[Route('/profil/', name: 'app_user_profil')]
+    public function index(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserRepository $userRepository
+    ): Response {
+        $user = $userRepository->find($this->getUser());
+
+        $form = $this->createForm(ProfilType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_profil', [], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('user/profil.html.twig', [
+            'form' => $form,
         ]);
     }
 }
